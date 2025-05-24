98423ae8214b6626c5c2385d701e3c09e43a8a5c
Fauzan <zanf518@gmail.com>
2025-05-24 18:38:11 +0800

    dotenv, perubahan config + env

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: 'root',
+    password: null,
+    database: 'arsipnowdb',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    timezone: '+07:00',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    timezone: '+07:00',
+  }
+};
